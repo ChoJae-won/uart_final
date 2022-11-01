4f1d53642f4580fa442a1a5ebd7e8c6e597024a7
ChoJae-won <jwcho1996@gmail.com>
2022-11-01 22:41:59 +0900

    first commit

---
+++ uart_final/src/main/java/com/multi/UartFinalApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UartFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UartFinalApplication.class, args);
+	}
+
+}
